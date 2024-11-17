bd636a372b34ae0dad658806b166c55195772156
ONLOX <3316820553@qq.com>
2024-11-17 19:11:32 +0800

    feat: traj_executor of Unitree

--- src/arm_motor_interface/scripts/traj_executor.py
+++ src/arm_motor_interface/scripts/traj_executor.py
@@ -17,7 +17,7 @@ class Traj_executor:
     def __init__(self):
         self.traj_subscriber = rospy.Subscriber("/arm_controller/follow_joint_trajectory/goal", FollowJointTrajectoryActionGoal, self.traj_goal_callback)
         self.joint_state_pub = rospy.Publisher('joint_states', JointState, queue_size=1)
-        # self.unitree_joint_state_sub = rospy.Subscriber('unitree_joint_states', JointState, self.unitree_joint_state_callback)
+        self.unitree_joint_state_sub = rospy.Subscriber('unitree_joint_states', JointState, self.unitree_joint_state_callback)
         self.joint_angles = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         self.joint_names = ['joint1', 'joint2', 'joint3', 'joint4', 'joint5', 'joint6']
         self.joint_state = JointState()
@@ -30,10 +30,10 @@ class Traj_executor:
         self.CyberGear_init()
         self.timer = rospy.Timer(rospy.Duration(0.1), self.timer_callback)
 
-    # def unitree_joint_state_callback(self, msg):
-    #     self.joint_angles[0] = msg.position[0]
-    #     self.joint_angles[1] = msg.position[1]
-    #     self.joint_angles[2] = msg.position[2]
+    def unitree_joint_state_callback(self, msg):
+        self.joint_angles[0] = msg.position[0]
+        self.joint_angles[1] = msg.position[1]
+        self.joint_angles[2] = msg.position[2]
 
     def CyberGear_init(self):
         self.cybergear_motor_controller = CyberGear.MotorController('/dev/ttyUSB0', 921600, timeout=1)
@@ -74,8 +74,6 @@ class Traj_executor:
             if now < self.time_from_start[i]:
                 break
             counter = i
-        print(len(self.time_from_start))
-        print(counter)
 
         # joint 4
         control_mode_msg_1 = CyberGear.ControlModeMsg()
@@ -88,9 +86,9 @@ class Traj_executor:
         feedback_msg_1 = self.cybergear_motor_controller.controlMode(control_mode_msg_1)
         if feedback_msg_1 is not None:
             if feedback_msg_1.position < 0:
-                self.joint_angles[3] = -((-feedback_msg_1.position) % (2*np.pi))
+                self.joint_angles[3] = -((-feedback_msg_1.position) % 6.28)
             else:
-                self.joint_angles[3] = feedback_msg_1.position % (2*np.pi)
+                self.joint_angles[3] = feedback_msg_1.position % 6.28
         # joint 5
         control_mode_msg_2 = CyberGear.ControlModeMsg()
         control_mode_msg_2.can_id   = 2
@@ -102,9 +100,9 @@ class Traj_executor:
         feedback_msg_2 = self.cybergear_motor_controller.controlMode(control_mode_msg_2)
         if feedback_msg_2 is not None:
             if feedback_msg_2.position < 0:
-                self.joint_angles[4] = -((-feedback_msg_2.position) % (2*np.pi))
+                self.joint_angles[4] = -((-feedback_msg_2.position) % 6.28)
             else:
-                self.joint_angles[4] = feedback_msg_2.position % (2*np.pi)
+                self.joint_angles[4] = feedback_msg_2.position % 6.28
 
         self.joint_state.header.stamp = rospy.Time.now()
         self.joint_state.name = self.joint_names
--- src/arm_motor_interface/src/unitree_traj_executor.cpp
+++ src/arm_motor_interface/src/unitree_traj_executor.cpp
@@ -1,48 +1,116 @@
 ﻿#include <ros/ros.h>
 #include <std_msgs/Int64.h>
 #include <std_srvs/SetBool.h>
+#include <vector>
 
 #include "sensor_msgs/JointState.h"
 #include "control_msgs/FollowJointTrajectoryActionGoal.h"
-#include <vector>
+#include "serialPort/SerialPort.h"
 
 
-class Unitree_Traj_executor {
+class Unitree_Traj_executor
+{
     private:
     ros::Publisher joint_state_pub;
     ros::Subscriber unitree_traj_sub;
     ros::Timer traj_executor;
-    double joint_angles[6] = {0, 0, 0, 0, 0, 0}; // 6 joint angles
+    double joint_angles[3] = {0, 0, 0}; // 6 joint angles
     std::vector<std::vector<double>> positions;
     std::vector<std::vector<double>> velocities;
     std::vector<std::vector<double>> accelerations;
     std::vector<double> time_from_start;
+    SerialPort serial;
 
     public:
     Unitree_Traj_executor(ros::NodeHandle *nh) 
     {
         joint_state_pub = nh->advertise<sensor_msgs::JointState>("/unitree_joint_states", 1);    
         unitree_traj_sub = nh->subscribe("/arm_controller/follow_joint_trajectory/goal", 1, &Unitree_Traj_executor::traj_goal_callback, this);
-        traj_executor = nh->createTimer(ros::Duration(0.1), std::bind(&Unitree_Traj_executor::timer_callback, this));
+        
         positions.push_back(std::vector<double>{0.0, 0.0, 0.0, 0.0, 0.0, 0.0});
         velocities.push_back(std::vector<double>{0.0, 0.0, 0.0, 0.0, 0.0, 0.0});
         accelerations.push_back(std::vector<double>{0.0, 0.0, 0.0, 0.0, 0.0, 0.0});
         time_from_start.push_back(0);
 
+        serial = SerialPort("/dev/ttyUSB0");
+        traj_executor = nh->createTimer(ros::Duration(0.1), std::bind(&Unitree_Traj_executor::timer_callback, this));
     }
 
     void timer_callback()
     {
-        /* The Timer callback */
+        ros::Time now = ros::Time::now();
+        int counter = 0;
+        for (int i = 0; i < time_from_start.size(); i++)
+        {
+            if (now.toSec() < time_from_start[i])
+                break;
+            counter = i;
+        }
+
+        // joint 1
+        MotorCmd cmd_1;
+        MotorData data_1;
+        cmd_1.motorType = MotorType::GO_M8010_6;
+        cmd_1.id = 0;
+        cmd_1.mode = 1;
+        cmd_1.T = 0.0;
+        cmd_1.W = this->velocities[counter][0] * 6.33;
+        cmd_1.Pos = this->positions[counter][0];
+        cmd_1.K_P = 0.1;
+        cmd_1.K_W = 0.05;
+        this->serial.sendRecv(&cmd_1, &data_1);
+        if (data_1.correct)
+        {
+            if (data_1.Pos < 0)
+                joint_angles[0] = -((-data_1.Pos) % 6.28);
+            else
+                joint_angles[0] = data_1.Pos % 6.28;
+        }
+        // joint 2
+        MotorCmd cmd_2;
+        MotorData data_2;
+        cmd_2.motorType = MotorType::GO_M8010_6;
+        cmd_2.id = 1;
+        cmd_2.mode = 1;
+        cmd_2.T = 0.0;
+        cmd_2.W = this->velocities[counter][1] * 6.33;
+        cmd_2.Pos = this->positions[counter][1];
+        cmd_2.K_P = 0.1;
+        cmd_2.K_W = 0.05;
+        this->serial.sendRecv(&cmd_2, &data_2);
+        if (data_2.correct)
+        {
+            if (data_2.Pos < 0)
+                joint_angles[1] = -((-data_2.Pos) % 6.28);
+            else
+                joint_angles[1] = data_2.Pos % 6.28;
+        }
+        // joint 3
+        MotorCmd cmd_3;
+        MotorData data_3;
+        cmd_3.motorType = MotorType::GO_M8010_6;
+        cmd_3.id = 2;
+        cmd_3.mode = 1;
+        cmd_3.T = 0.0;
+        cmd_3.W = this->velocities[counter][2] * 6.33;
+        cmd_3.Pos = this->positions[counter][2];
+        cmd_3.K_P = 0.1;
+        cmd_3.K_W = 0.05;
+        this->serial.sendRecv(&cmd_3, &data_3);
+        if (data_3.correct)
+        {
+            if (data_3.Pos < 0)
+                joint_angles[2] = -((-data_3.Pos) % 6.28);
+            else
+                joint_angles[2] = data_3.Pos % 6.28;
+        }
+
         sensor_msgs::JointState msg;
         msg.header.stamp = ros::Time::now();
         msg.name = {"motor1", "motor2", "motor3"};
-        for (int i = 0; i < 3; i++) {
-            msg.position.push_back(joint_angles[i]);
-        }
+        msg.position = {joint_angles[0], joint_angles[1], joint_angles[2]};
         joint_state_pub.publish(msg);
     }
-
 
     void traj_goal_callback(const control_msgs::FollowJointTrajectoryActionGoal::ConstPtr &msg) 
     {
@@ -58,6 +126,8 @@ class Unitree_Traj_executor {
         }   
     }
 };
+
+
 int main (int argc, char **argv)
 {
     ros::init(argc, argv, "number_counter");
