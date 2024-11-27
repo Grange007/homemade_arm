5e3eec04e3ba543e46bf76b505d93f3daa51e65d
WORMMMMMM <zhangzm22@mails.tsinghua.edu.cn>
2024-11-28 00:00:35 +0800

    Working for one

--- collector/easyrobot/easyrobot/encoder/test_encoder.py
+++ collector/easyrobot/easyrobot/encoder/test_encoder.py
@@ -1,8 +1,10 @@
 import numpy as np
+import sys
+sys.path.append('f:/清清电子资料库/学习相关/大三上/电子系统设计/code/collector/easyrobot')
 
 from easyrobot.encoder.End_effector import EndEffectorEncoder
 
-encoder = EndEffectorEncoder(ids = [2], port = '/dev/ttyUSB0', baudrate = 115200)
+encoder = EndEffectorEncoder(ids = [2], port = 'COM4', baudrate = 115200)
 
 while True:
     e = encoder.fetch_info()
