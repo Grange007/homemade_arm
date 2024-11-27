4913f38802e7338489ea779d793b3b55955b6afe
ONLOX <3316820553@qq.com>
2024-11-27 07:26:17 -0800

    fix: Unitree example

--- src/arm_motor_interface/motor_tools/Unitree/example.py
+++ src/arm_motor_interface/motor_tools/Unitree/example.py
@@ -3,23 +3,25 @@ import Unitree
 
 motor_controller = Unitree.MotorController('/dev/ttyUSB0', 921600, timeout=1)
 
-control_msg = Unitree.ControlMsg()
-control_msg.id       = 0
-control_msg.status   = 1
-control_msg.torque   = 0.0
-control_msg.position = 0.0
-control_msg.velocity = 0.0
-control_msg.Kp       = 0.0
-control_msg.Kw       = 0.0
-feedback_msg = motor_controller.control(control_msg)
-if feedback_msg != None:
-    print("id:      ", feedback_msg.id)
-    print("status:  ", feedback_msg.status)
-    print("torque:  ", feedback_msg.torque)
-    print("position:", feedback_msg.position)
-    print("velocity:", feedback_msg.velocity)
-    print("Kp:      ", feedback_msg.Kp)
-    print("Kw:      ", feedback_msg.Kw)
+for _ in range(50):
+    control_msg = Unitree.ControlMsg()
+    control_msg.id       = 0
+    control_msg.status   = 1
+    control_msg.torque   = 0.0
+    control_msg.position = 0.0
+    control_msg.velocity = 0.0
+    control_msg.Kp       = 0.0
+    control_msg.Kw       = 0.0
+    feedback_msg = motor_controller.control(control_msg)
+    if feedback_msg != None:
+        print("id:      ", feedback_msg.id)
+        print("status:  ", feedback_msg.status)
+        print("torque:  ", feedback_msg.torque)
+        print("position:", feedback_msg.position)
+        print("velocity:", feedback_msg.velocity)
+        print("Kp:      ", feedback_msg.Kp)
+        print("Kw:      ", feedback_msg.Kw)
+    time.sleep(0.1)
 
 # Close the serial port
 motor_controller.close()
